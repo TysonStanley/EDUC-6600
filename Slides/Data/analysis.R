c33d0015c5527294ca5ed504109e29d8db1aa4a1
Tyson S. Barrett <t.barrett88@gmail.com>
2018-03-14 16:20:42 -0600

    updated chapters

---
+++ Slides/Data/analysis.R
@@ -0,0 +1,81 @@
+library(tidyverse)
+
+df <- data_frame(
+  outcome = c(8,10,9,10,9,7,8,5,8,5,4,8,7,5,7),
+  group   = c(rep(1, 5), rep(2, 5), rep(3, 5))
+) %>%
+  mutate(group = factor(group, labels = c("A", "B", "C")))
+
+df %>%
+  group_by(group) %>%
+  furniture::table1(outcome)
+df %>%
+  ggplot(aes(group, outcome)) +
+  geom_jitter(alpha = .5) +
+  geom_boxplot(alpha = .5) +
+  theme_minimal()
+
+df %>%
+  car::leveneTest(outcome ~ group,
+                  data = .,
+                  center = mean)
+
+df %>%
+  aov(outcome ~ group, 
+      data = .) %>%
+  summary()
+
+df %>%
+  dplyr::group_by(group) %>%
+  summarize(means = mean(outcome),
+            ses   = sd(outcome)/sqrt(n())) %>%
+  ggplot(aes(group, means)) +
+    geom_point() +
+    geom_errorbar(aes(ymin = means - 2*ses, ymax = means + 2*ses),
+                  width = .2)
+
+  
+  
+## Chapter 14 Example Data
+
+library(tidyverse)
+
+df <- read_csv("Slides/Data/data_ch14.csv") %>%
+  mutate(Group = factor(Group)) %>%
+  na.omit %>%
+  as.tibble
+df
+
+df %>%
+  tidyr::gather("Drug", "value", 2:4) %>%
+  group_by(Group, Drug) %>%
+  summarize(marginal_means = mean(value))
+
+df %>%
+  tidyr::gather("Drug", "value", 2:4) %>%
+  dplyr::group_by(Group) %>%
+  dplyr::summarize(marginal_means = mean(value)) %>%
+  ggplot(aes(Group, marginal_means)) +
+  geom_line() +
+  geom_point()
+
+df %>%
+  tidyr::gather("Drug", "value", 2:4) %>%
+  dplyr::group_by(Drug) %>%
+  dplyr::summarize(marginal_means = mean(value)) %>%
+  ggplot(aes(Drug, marginal_means)) +
+  geom_line() +
+  geom_point()
+
+df %>%
+  tidyr::gather("Drug", "value", 2:4) %>%
+  dplyr::group_by(Group, Drug) %>%
+  dplyr::summarize(marginal_means = mean(value)) %>%
+  ggplot(aes(Drug, marginal_means, group = Group, color = Group)) +
+    geom_line() +
+    geom_point()
+
+
+
+
+
---
+++ anova_functions.R
@@ -0,0 +1,130 @@
+
+
+set.seed(42)
+z <- data.frame(a1 = c(rnorm(100,2), rnorm(100,1),rnorm(100,0)),
+                b1 = rep(c("A", "B", "C"), each = 100),
+                c1 = factor(rbinom(300, 1, .5)),
+                ID = 1:300,
+                a2 = c(rnorm(100,2), rnorm(100,1),rnorm(100,0)),
+                a3 = c(rnorm(100,2), rnorm(100,1),rnorm(100,0)))
+
+
+
+anoRM <- function(data, dep, rm_lab = NULL, covariates = 1, ss = "III", adjust = "none"){
+  
+  data <- data.frame(data)
+  
+  if (is.null(rm_lab)){
+    repeated <- paste0("time", 1:length(dep))
+  } else {
+    repeated <- rm_lab
+  }
+  
+  dep1 <- data[, dep] %>% as.matrix
+  form <- as.formula(paste("dep1 ~", paste(covariates, collapse = " + ")))
+  mlm1 <- lm(form, data = data)
+  mlm1.aov <- car::Anova(mlm1, 
+                         idata = data.frame(repeated),
+                         idesign = ~repeated, 
+                         type = ss)
+  main <- summary(mlm1.aov, multivariate=FALSE)
+  
+  ps <- diff <- vector("numeric", length(dep)-1)
+  nam <- vector("character", length(dep)-1)
+  for (i in 1:(length(dep)-1)){
+    nam[i]  <- paste(dep[i:(i+1)], collapse = " vs. ")
+    diff[i] <- t.test(data[, dep[i]], 
+                       data[, dep[i+1]],
+                       paired = TRUE)$estimate
+    ps[i]   <- t.test(data[, dep[i]], 
+                      data[, dep[i+1]],
+                      paired = TRUE)$p.value
+  }
+  nam[length(nam)+1]   <- paste(dep[c(1, length(dep))], collapse = " vs. ")
+  diff[length(diff)+1] <- t.test(data[, dep[1]], 
+                                 data[, dep[length(dep)]],
+                                 paired = TRUE)$estimate
+  ps[length(ps)+1]     <- t.test(data[, dep[1]], 
+                                 data[, dep[length(dep)]],
+                                 paired = TRUE)$p.value
+  
+  post <- switch(adjust,
+                 "none" = data.frame(comp = nam, diff = diff, pvalue = ps),
+                 "bonf" = data.frame(comp = nam, diff = diff, pvalue = ps/length(ps)))
+  
+  means <- lsmeans::lsmeans(mlm1, 
+                            specs = c(paste(attr(mlm1$terms, "variables"))[3:(length(covariates)+2)]))
+
+  list("ANOVArm" = main, "PostHoc" = post, "LSmeans" = means)
+}
+ano <- function(formula, data, ss = "III", posthoc = "tukey", adjust = "none", contrasts = NULL){
+  call <- match.call()
+  
+  fit <- aov(formula, data, contrasts = contrasts)
+  
+  an_tab <- car::Anova(fit, type = ss)
+  post <- switch(posthoc,
+                 tukey = TukeyHSD(fit),
+                 t.test = pairwise.t.test(fit, p.adj = adjust))
+  
+  out <- list("ANOVA" = an_tab, 
+              "PostHoc" = post,
+              "aovCall" = call,
+              "residuals" = fit$residuals,
+              "preds" = fit$fitted.values,
+              "contrasts" = fit$contrasts,
+              "levels" = fit$xlevels)
+  class(out) = "ano"
+  out
+  
+}
+anoRM(z, dep = c("a1", "a2", "a3"), covariates = c("b1", "c1"))
+
+
+
+
+
+
+print.ano <- function(x, ...){
+  cat("-----\n",
+      "Analysis of Variance\n",
+      " - Formula:", deparse(x$aovCall$formula), "\n",
+      " -", length(x$levels), "factors (", paste0(sapply(x$levels, length), collapse = " x "), ")\n",
+      " - Contrasts:", paste0(x$contrasts, collapse = ", "), "\n",
+      "\n\n")
+  x$ANOVA %>%
+    data.frame %>%
+    round(5) %>% print
+  
+  cat("\n\n")
+  print(x$PostHoc)
+  
+}
+
+ano(a ~ b + c, x)
+
+aov_car
+
+library(afex)
+z %>%
+  aov_ez("ID", dv = "a1", between = "b1", data = .) 
+z %>%
+  aov_4(a1 ~ b1 + (1|ID), data = .) 
+
+
+z %>%
+  gather("time", "value", a1, a2, a3) %>%
+  data.frame %>%
+  aov_4(value ~ 1 + (time|ID),
+        data = .)
+
+z %>%
+  gather("time", "value", a1, a2, a3) %>%
+  lme4::lmer(value ~ time + (1|ID),
+             data = .)
+
+
+
+library(tidyverse)
+
+glimpse(z)
